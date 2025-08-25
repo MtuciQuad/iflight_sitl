5f8150be7f23f0800a84c80764b7db6d964dd669
M1NT <ilya.ablyazov.2004@gmail.com>
2025-08-25 13:07:37 +0300

    rc reconnection and new camera pose

--- rust_utility/src/bin/rc.rs
+++ rust_utility/src/bin/rc.rs
@@ -1,4 +1,4 @@
-use hidapi;
+use hidapi::{HidApi, HidResult};
 use gz::{msgs::float_v::Float_V, transport::Node};
 use std::time::Duration;
 use std::process::Command;
@@ -24,32 +24,37 @@ fn process_data(raw_data: &[u8;19]) -> [f32; 8] {
 }
 
 fn main() {
-    let api = hidapi::HidApi::new().unwrap();
+    let mut api = HidApi::new().unwrap();
     let (vid, pid) = (4617, 20308);
-    let device = api.open(vid, pid).unwrap();
     let mut buf = [0u8; 19];
-    let mut last_SD = 0u8;
-
-    // Node for publishing rc data on RcData topic
     let mut node = Node::new().unwrap();
     let mut publisher: gz::transport::Publisher<Float_V> = node.advertise("RcData").unwrap();
-
     loop {
-        let _res = device.read(&mut buf[..]).unwrap();
-        let data = process_data(&buf);
-        let topic = Float_V {
-            data: data.to_vec(),
-            ..Default::default()
-        };
-        if last_SD == 0 && data[7] != 0.0 {
-            Command::new("gz")
-            .args(["service", "-s", "/world/iflight_world/control", "--reqtype", "gz.msgs.WorldControl", "--reptype", "gz.msgs.Boolean", "--req", "reset: {all: true}"])
-            .output().expect("Error");
+        if let HidResult::Ok(device) = api.open(vid, pid) {
+            println!("Device was connected");
+            let mut last_sd = 0u8;
+            loop {
+                if let Err(_) =  device.read(&mut buf[..]) {
+                    println!("Device was disconnected");
+                    break
+                }
+                // let _res = device.read(&mut buf[..]).unwrap();
+                let data = process_data(&buf);
+                let topic = Float_V {
+                    data: data.to_vec(),
+                    ..Default::default()
+                };
+                if last_sd == 0 && data[7] != 0.0 {
+                    Command::new("gz")
+                    .args(["service", "-s", "/world/iflight_world/control", "--reqtype", "gz.msgs.WorldControl", "--reptype", "gz.msgs.Boolean", "--req", "reset: {all: true}"])
+                    .output().expect("Error");
+                }
+                // Publish the rc data
+                assert!(publisher.publish(&topic));
+                last_sd = data[7] as u8;
+                api.refresh_devices();
+                std::thread::sleep(Duration::from_micros(250));
+            }
         }
-        // Publish the rc data
-        assert!(publisher.publish(&topic));
-        last_SD = data[7] as u8;
-        std::thread::sleep(Duration::from_micros(250));
     }
-
 }
